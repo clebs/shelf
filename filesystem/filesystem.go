8969932ff7454bffe1f68e9d93cdfea2aaf385c4
clebs <borja.clemente@gmail.com>
2017-02-09 10:01:22 +0100

    Added new line after removal confirmation

--- filesystem/filesystem.go
+++ filesystem/filesystem.go
@@ -44,7 +44,7 @@ func Delete(name string) {
 	file := getProjectFilePath(name)
 	err := os.Remove(file)
 	errorout.ErrQuit(err, "Could not delete project")
-	fmt.Printf("The project %s has been removed from the shelf.", name)
+	fmt.Printf("The project %s has been removed from the shelf.\n", name)
 }
 
 func getProjectFilePath(name string) string {
